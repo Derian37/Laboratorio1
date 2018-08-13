02b66ed6e012683b55bf40f5be66a436d57c1933
Derian37 <32728553+Derian37@users.noreply.github.com>
2018-08-13 15:42:42 -0600

    penes x2

--- js/function.js
+++ js/function.js
@@ -1,23 +1,19 @@
-<!doctype html>
 function armstr()
 {
 var arm=0,a,b,c,d,num;
 num=Number(document.getElementById("no_input").value);
 
 temp=num;
-while(temp>0)
-{
+while(temp>0){
 a=temp%10;
 temp=parseInt(temp/10); // convert float into Integer
 arm=arm+a*a*a;
 }
-if(arm==num)
-{
-alert("Es un Numero de Armstromg");
+if(arm==num){
+document.getElementById("res").value ="Es un Numero de Armstromg"; 
 }
-else
-{
-alert("No es un numero de Armstrong");
+else{
+document.getElementById("res").value ="No es un Numero de Armstromg"; 
 }
 }
 
